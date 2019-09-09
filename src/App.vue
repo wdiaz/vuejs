35b7b94eabc6e4656f6a936f2948bdaa80f75816
Walter Diaz <wdiaz503@gmail.com>
2019-09-08 20:57:59 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <AddTodo v-on:add-todo="addTodo"/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from "./components/AddTodo";
+import axios from 'axios';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    Todos,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: [
+
+      ]
+    }
+  },
+  methods: {
+
+    deleteTodo(id) {
+      //this.todos = this.todos.filter(todo => todo.id !== id)
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+              .then(res => this.todos = this.todos.filter( todo => todo.id !== id))
+              .catch(e => console.log(e))
+    },
+
+    addTodo(newTodo) {
+        const {title, completed} = newTodo;
+        axios.post('https://jsonplaceholder.typicode.com/todos', {
+          title: title,
+          completed: completed
+        })
+          .then( res => this.todos = [...this.todos, res.data])
+          .catch( e => console.log(e));
+    }
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+            .then(res => this.todos = res.data)
+            // eslint-disable-next-line no-console
+            .catch(e => console.log(e))
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+   body {
+     font-family: Arial, Helvetica, sans-serif;
+     line-height: 1.4;
+   }
 </style>
